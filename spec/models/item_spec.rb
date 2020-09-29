bd53630960782ffea81764a75a212eb4c48ed166
Daniel <danielnottingham.ti@gmail.com>
2020-09-29 11:40:41 -0300

    issue - add the user entity with the email field#26 typed email valid

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  let(:user) { User.create(:user) }
 
   describe 'not empty' do
     it { is_expected.to validate_presence_of(:key) }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
   describe 'not empty' do
     it { is_expected.to validate_presence_of(:email) }
   end
@@ -11,5 +12,17 @@ RSpec.describe User, type: :model do
 
   describe 'validates uniqueness of email' do
     it { is_expected.to validate_uniqueness_of(:email) }
+  end
+
+  describe 'valid email typed' do
+    it 'should be valid email typed like ' do
+      email = User.new(email: 'valid@email.com')
+      expect(email).to be_valid
+    end
+
+    it 'should not be valid email typed like ' do
+      email2 = User.new(email: 'invalid@invalid@.com')
+      expect(email2).to_not be_valid
+    end
   end
 end
